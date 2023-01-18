4a13b30ef055874c1cd561545bcbe3e43b32603d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-01-18 17:13:41 +0100

    Create CreateProjectLink.jsx
    react project for front end

---
+++ front-end/src/components/CreateProjectLink.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function CreateProjectLink() {
+    return (
+        <Link to="/createproject" className="createProject-link">Créer un projet</Link>
+    )
+}
