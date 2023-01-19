8384edb4ec28d68aae204e3283e3cb4cae0c8d8c
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-01-19 17:00:35 +0100

    Update Projects.jsx
    Get request coded.

--- front-end/src/pages/Projects.jsx
+++ front-end/src/pages/Projects.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 export default function Projects() {
+
+  const { dataProjects, setDataProjets } = useState([]);
+
+  const API_URL_Plano = "http://localhost:8080/plano/projects";
+
+  useEffect(() => {
+    axios.get(API_URL_Plano)
+      .then(
+        res => {
+          const occurence = res.data;
+          setDataProjets(occurence);
+        }
+      )
+  }, []);
+
+  // code in to the id for useParams for component OneProject
+
   return (
     <div>Hello world Projects!</div>
   )
