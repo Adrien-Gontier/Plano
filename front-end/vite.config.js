9deacd03fb4e647570b180632ab6c90b9031eab8
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-01-20 15:54:39 +0100

    Update vite.config.js
    code improved

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {port: 5173},
+  server: { port: 5173 },
   plugins: [react()],
-})
+});
