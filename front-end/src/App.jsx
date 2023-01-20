849f85dce4370261d4dac2d613703185524fc7e8
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-01-20 15:47:36 +0100

    Update App.jsx
    Routes added and code improved

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,24 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './assets/sass/App.css'
-import Header from './components/Header'
-import CreateProject from './pages/CreateProject'
-import CreateTask from './pages/CreateTask'
-import OneProject from './pages/OneProject'
-import Projects from './pages/Projects'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./assets/sass/App.css";
+import Header from "./components/Header";
+import CreateProject from "./pages/CreateProject";
+import CreateTask from "./pages/CreateTask";
+import OneProject from "./pages/OneProject";
+import AllProjects from "./pages/AllProjects";
 
-function App() {
-
+export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Projects />} />
+        <Route path="/" element={<AllProjects />} />
         <Route path="/createproject" element={<CreateProject />} />
         <Route path="/createtask" element={<CreateTask />} />
-        <Route path="/oneproject/:id" element={<OneProject />} />
+        <Route path="/oneproject/:idProject" element={<OneProject />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
-export default App
