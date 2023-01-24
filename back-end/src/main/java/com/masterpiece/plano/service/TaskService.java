68b9c6b16e1c6b9df57f1c347267c47d1a34b28d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-01-24 14:04:58 +0100

    Update TaskService.java
    Bug fixed

--- back-end/src/main/java/com/masterpiece/plano/service/TaskService.java
+++ back-end/src/main/java/com/masterpiece/plano/service/TaskService.java
@@ -1,15 +1,16 @@
 package com.masterpiece.plano.service;
 
+import com.masterpiece.plano.dtos.TaskDto;
 import com.masterpiece.plano.entity.Task;
 
 public interface TaskService {
     Iterable getAllTasks();
 
-    Task getTaskById(String taskId);
+    TaskDto getTaskById(String taskId);
 
     Task createTask(Task taskRequest);
 
-    Task updateTask(String taskId, Task taskRequest);
+    TaskDto updateTask(String taskId, Task taskRequest);
 
     String deleteTask(String taskId);
 }
