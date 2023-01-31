31b60feb1b84099eb6a68773ae22e056ef35ba83
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-01-31 16:51:36 +0100

    Update Title.jsx
    name of application updated

--- front-end/src/components/Title.jsx
+++ front-end/src/components/Title.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Title() {
-  return <Link to='/' className="title-app">Plano</Link>;
+  return <Link to='/' className="title-app">P|_A|\|O</Link>;
 }
