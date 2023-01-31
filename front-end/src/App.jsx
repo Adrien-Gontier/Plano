ab36699ea2c100acf24f8bd579e292c85291b353
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-01-31 16:49:26 +0100

    Update App.jsx
    New routes added

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,17 +6,21 @@ import CreateTask from "./pages/CreateTask";
 import OneProject from "./pages/OneProject";
 import AllProjects from "./pages/AllProjects";
 import OneTask from "./pages/OneTask";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<AllProjects />} />
         <Route path="/createproject" element={<CreateProject />} />
         <Route path="/createtask/:idProject" element={<CreateTask />} />
         <Route path="/oneproject/:idProject" element={<OneProject />} />
         <Route path="/onetask/:idTask" element={<OneTask />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
       </Routes>
     </BrowserRouter>
   );
