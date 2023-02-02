f64e5e4953063c3dc78d2fab8bff5b29e5a6e64e
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-02 16:36:56 +0100

    Update User.java
    Security and user management added

--- back-end/src/main/java/com/masterpiece/plano/entity/User.java
+++ back-end/src/main/java/com/masterpiece/plano/entity/User.java
@@ -1,20 +1,103 @@
 package com.masterpiece.plano.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.hibernate.annotations.GenericGenerator;
 
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@JsonIdentityInfo(generator= ObjectIdGenerators.IntSequenceGenerator.class)
 @Entity
-@Table(name = "`user_entity`")
+@Table(name = "`user`")
 public class User {
     @Id
-    private Long id;
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String userId;
 
-    public void setId(Long id) {
-        this.id = id;
+    @Column(unique = true, nullable = false)
+    private String identifier;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public User() {
     }
 
-    public Long getId() {
-        return id;
+    public User(String userId, String identifier, String password, String firstName, String lastName, Set<Role> roles) {
+        this.userId = userId;
+        this.identifier = identifier;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.roles = roles;
     }
+
+
+
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
