444943963007bb3f3907d6a77b981cc90274f135
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-02 16:36:24 +0100

    Create RoleRepository.java
    Security and user management added

---
+++ back-end/src/main/java/com/masterpiece/plano/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.masterpiece.plano.repository;
+
+import com.masterpiece.plano.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RoleRepository extends JpaRepository<Role, String> {
+}
