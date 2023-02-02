702293b47a49781742693ad3a262a3d3fa0a119d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-02 16:38:34 +0100

    Update Login.jsx
    Axios post request added and catch Token added

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Title from '../components/Title'
 
 export default function Login() {
-  const [identifier, setIdentifier] = useState('')
-  const [password, setPassword] = useState('')
+  const [identifierValue, setIdentifierValue] = useState('')
+  const [passwordValue, setPasswordValue] = useState('')
+  const [actionLogin, setActionLogin] = useState(false)
+
+  const apiUrlPlano = 'http://localhost:3000/plano/login'
+
+    useEffect(() => {
+        if (actionLogin) {
+            axios
+                .post(apiUrlPlano, {
+                    identifier: identifierValue,
+                    password: passwordValue
+                })
+                .then(function (response) {
+                  console.log(response.data);
+                })
+              //   .catch(function (error) {
+              //     console.log(error);
+              //   });
+            setActionLogin(false)
+        }
+    }, [actionLogin])
+
   return (
     <div className='login'>
       <div className='login__header'>
@@ -23,9 +45,9 @@ export default function Login() {
             <label>
               <input
                 type="text"
-                value={identifier}
+                value={identifierValue}
                 placeholder="Veuillez renseigner votre identifiant"
-                onChange={(event) => setIdentifier(event.target.value)}
+                onChange={(event) => setIdentifierValue(event.target.value)}
               />
             </label>
           </div>
@@ -34,13 +56,13 @@ export default function Login() {
             <label>
               <input
                 type="text"
-                value={password}
+                value={passwordValue}
                 placeholder="Veuillez renseigner votre identifiant"
-                onChange={(event) => setPassword(event.target.value)}
+                onChange={(event) => setPasswordValue(event.target.value)}
               />
             </label>
           </div>
-          <button>Valider</button>
+          <button onClick={() => setActionLogin(true)}>Valider</button>
         </div>
       </div>
     </div>
