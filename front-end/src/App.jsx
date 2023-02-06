345c50a98a3c2fec847035e00ed5b21405eafde8
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-06 16:19:45 +0100

    Update App.jsx
    Code factorized and login added

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import "./assets/sass/App.css";
 import Header from "./components/Header";
 import CreateProject from "./pages/CreateProject";
@@ -8,18 +8,61 @@ import AllProjects from "./pages/AllProjects";
 import OneTask from "./pages/OneTask";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 export default function App() {
+
+  const [isLogged, setIsLogged] = useState(false);
+  const [apiUrl, setApiUrl] = useState('');
+
+  const token = localStorage.getItem("TOKEN")
+  let source = axios.CancelToken.source()
+  const [dataProjects, setDataProjects] = useState([])
+
+  useEffect(() => {
+    if (localStorage.getItem("TOKEN") != null) {
+      axios
+        .get(apiUrl, {
+          cancelToken: source.token,
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        .then((response) => {
+          const occurence = response.data
+          
+          setTimeout(() => {
+            setDataProjects(occurence)
+            setIsLogged(true)
+          }, 2000)
+        })
+        .catch((error) => {
+          if (error?.code == "ERR_CANCELED") {
+            // request canceled, do nothing
+          } else if (error?.response?.status == 401) {
+            // toast("Unauthorized access");
+            navigate("/");
+          } else {
+            console.error(error);
+          }
+        })
+    }
+  }, [apiUrl]);
+
+  const header = isLogged == false ? null : <Header isLogged={isLogged} setIsLogged={setIsLogged} />
+
   return (
     <BrowserRouter>
-      <Header />
+      {header}
       <Routes>
-        <Route path="/" element={<AllProjects />} />
+        <Route path="/" element={<Login isLogged={isLogged} setIsLogged={setIsLogged} />} />
+        <Route path="/allprojects" element={<AllProjects apiUrl={apiUrl} setApiUrl={setApiUrl} props={dataProjects} />} />
         <Route path="/createproject" element={<CreateProject />} />
         <Route path="/createtask/:idProject" element={<CreateTask />} />
-        <Route path="/oneproject/:idProject" element={<OneProject />} />
-        <Route path="/onetask/:idTask" element={<OneTask />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/oneproject/:idProject" element={<OneProject apiUrl={apiUrl} setApiUrl={setApiUrl} props={dataProjects} />} />
+        <Route path="/onetask/:idTask" element={<OneTask apiUrl={apiUrl} setApiUrl={setApiUrl} props={dataProjects} />} />
         <Route path="/registration" element={<Registration />} />
       </Routes>
     </BrowserRouter>
