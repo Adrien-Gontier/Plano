5c8bc9d013bf2a21df3ee8e493b3283f705dd601
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-06 16:31:38 +0100

    Update Header.jsx
    Header logout button improved

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import DeconnectBtn from './DeconnectBtn'
 import Title from './Title'
 
-export default function Header() {
+export default function Header(props) {
+  const { isLogged, setIsLogged } = props
   return (
     <div className="header">
       <Title />
-      <DeconnectBtn />
+      <DeconnectBtn
+        isLogged={isLogged}
+        setIsLogged={setIsLogged}
+      />
     </div>
   )
 }
