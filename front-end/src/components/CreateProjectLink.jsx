8d31dc273fe5392b08b58386fe53b4a57a8f4e18
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-06 16:28:11 +0100

    Update CreateProjectLink.jsx
    Indentation improved

--- front-end/src/components/CreateProjectLink.jsx
+++ front-end/src/components/CreateProjectLink.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function CreateProjectLink() {
   return (
-    <Link to="/createproject" className="createProject-link">
+    <Link
+      to="/createproject"
+      className="createProject-link">
       Créer un projet
     </Link>
   );
