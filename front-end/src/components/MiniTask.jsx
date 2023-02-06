ed14e1d3a934e793a8a2bded3421d7487c85e98e
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-06 16:33:34 +0100

    Update MiniTask.jsx
    Indentation improved

--- front-end/src/components/MiniTask.jsx
+++ front-end/src/components/MiniTask.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function MiniTask({ props }) {
     return (
         <div>
-            <p>{props.name}</p>
+            <p>
+                {props.name}
+            </p>
         </div>
     )
 }
