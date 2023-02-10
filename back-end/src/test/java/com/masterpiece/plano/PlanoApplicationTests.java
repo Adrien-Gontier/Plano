074659ac5ac4a7a008ed6d6337c5a23eb3bc9ed8
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-10 08:39:41 +0100

    Update PlanoApplicationTests.java
    unitary test for create project added

--- back-end/src/test/java/com/masterpiece/plano/PlanoApplicationTests.java
+++ back-end/src/test/java/com/masterpiece/plano/PlanoApplicationTests.java
@@ -1,13 +1,47 @@
 package com.masterpiece.plano;
 
+import com.masterpiece.plano.entity.Project;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class PlanoApplicationTests {
 
+
+
+	@SneakyThrows
 	@Test
-	void contextLoads() {
+	void testCreateProject() {
+
+		String projectId = "e9a43514-a881-11ed-afa1-0242ac120002";
+		String name = "Test-project";
+		String description = "the description";
+		String dateString = "2022-01-01";
+		Date dateDeadLine = new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
+		String whyIPassed = "I passed";
+		String whyIFailed = "I Failed";
+		String toDoBetter = "To do better";
+		Project project = new Project(projectId, name, description, dateDeadLine, whyIPassed, whyIFailed, toDoBetter);
+
+		assertEquals(projectId, project.getProjectId());
+		assertEquals(name, project.getName());
+		assertEquals(description, project.getDescription());
+		assertEquals(dateDeadLine, project.getDateDeadLine());
+		assertEquals(whyIPassed, project.getWhyIPassed());
+		assertEquals(toDoBetter, project.getToDoBetter());
 	}
+
+
 
 }
