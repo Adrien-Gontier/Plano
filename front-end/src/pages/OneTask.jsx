605649a8c3beac871113bf76a97997042d93d7ff
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-05-16 19:19:26 +0200

    Update OneTask.jsx
    Code format improved

--- front-end/src/pages/OneTask.jsx
+++ front-end/src/pages/OneTask.jsx
@@ -3,9 +3,8 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 export default function OneTask(props) {
-
     const { idTask } = useParams()
-    const navigate = useNavigate();
+    const navigate = useNavigate()
     const [actionDeleteButton, setActionDeleteButton] = useState(0)
 
     const { apiUrl, setApiUrl, dataProjects } = props
@@ -16,11 +15,9 @@ export default function OneTask(props) {
 
     useEffect(() => {
         if (actionDeleteButton == 2) {
-            axios
-                .delete(apiUrl)
-                .then((res) => {
-                    const occurence = res.data
-                })
+            axios.delete(apiUrl).then((res) => {
+                const occurence = res.data
+            })
             navigate(-1)
         }
     }, [actionDeleteButton])
@@ -29,12 +26,40 @@ export default function OneTask(props) {
         <div>
             <button onClick={() => navigate(-1)}>Retour</button>
             <p>{dataOneTask.name}</p>
-            {actionDeleteButton == 0 ? <button onClick={() => setActionDeleteButton(actionDeleteButton + 1)}>Supprimer la tâche</button> : null}
-            {actionDeleteButton == 1 ? <p>"Êtes-vous vraiment sur de vouloir supprimer cette tâche ?"</p> : null}
-            {actionDeleteButton == 1 ? <p>"Cette action entrainera la suppression de toutes les données de cette tâche"</p> : null}
-            {actionDeleteButton == 1 ? <p>"Si vous êtes sur cliquez une nouvelle fois sur le boutton"</p> : null}
-            {actionDeleteButton == 1 ? <button onClick={() => setActionDeleteButton(actionDeleteButton + 1)}>Supprimer la tâche</button> : null}
-
+            {actionDeleteButton == 0 ? (
+                <button
+                    onClick={() =>
+                        setActionDeleteButton(actionDeleteButton + 1)
+                    }
+                >
+                    Supprimer la tâche
+                </button>
+            ) : null}
+            {actionDeleteButton == 1 ? (
+                <p>
+                    "Êtes-vous vraiment sur de vouloir supprimer cette tâche ?"
+                </p>
+            ) : null}
+            {actionDeleteButton == 1 ? (
+                <p>
+                    "Cette action entrainera la suppression de toutes les
+                    données de cette tâche"
+                </p>
+            ) : null}
+            {actionDeleteButton == 1 ? (
+                <p>
+                    "Si vous êtes sur cliquez une nouvelle fois sur le boutton"
+                </p>
+            ) : null}
+            {actionDeleteButton == 1 ? (
+                <button
+                    onClick={() =>
+                        setActionDeleteButton(actionDeleteButton + 1)
+                    }
+                >
+                    Supprimer la tâche
+                </button>
+            ) : null}
         </div>
     )
 }
