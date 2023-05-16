f3e8e4ba34aeb69aa20cf3288d0e5c0a14b88407
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-05-16 19:19:16 +0200

    Update Login.jsx
    Code format improved

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -4,75 +4,80 @@ import { Link, useNavigate } from 'react-router-dom'
 import Title from '../components/Title'
 
 export default function Login(props) {
-  const navigate = useNavigate()
-  const [identifierValue, setIdentifierValue] = useState('')
-  const [passwordValue, setPasswordValue] = useState('')
-  const [actionLogin, setActionLogin] = useState(false)
-  const {isLogged, setIsLogged} = props
-  
+    const navigate = useNavigate()
+    const [identifierValue, setIdentifierValue] = useState('')
+    const [passwordValue, setPasswordValue] = useState('')
+    const [actionLogin, setActionLogin] = useState(false)
+    const { isLogged, setIsLogged } = props
 
-  const apiUrlPlano = 'http://localhost:3000/plano/login'
+    const apiUrlPlano = 'http://localhost:3000/plano/login'
 
     useEffect(() => {
-      if(localStorage.getItem("TOKEN")) {
-       navigate('/allprojects')
-      }
+        if (localStorage.getItem('TOKEN')) {
+            navigate('/allprojects')
+        }
         if (actionLogin) {
             axios
                 .post(apiUrlPlano, {
                     identifier: identifierValue,
-                    password: passwordValue
+                    password: passwordValue,
                 })
                 .then(function (response) {
-                  localStorage.setItem("TOKEN", response.data)
-                  setIsLogged(true)
-                  navigate("/allprojects")
+                    localStorage.setItem('TOKEN', response.data)
+                    setIsLogged(true)
+                    navigate('/allprojects')
                 })
                 .catch(function (error) {
-                  console.log(error);
-                });
+                    console.log(error)
+                })
             setActionLogin(false)
         }
     }, [actionLogin])
 
-  return (
-    <div className='login'>
-      <div className='login__header'>
-        <Title />
-        <p>Un outil de planification de projet personnel</p>
-      </div>
-      <div className='login__main'>
-        <div className='login__main__signUp'>
-          <Link to='/registration'>S'enregistrer</Link>
+    return (
+        <div className="login">
+            <div className="login__header">
+                <Title />
+                <p>Un outil de planification de projet personnel</p>
+            </div>
+            <div className="login__main">
+                <div className="login__main__signUp">
+                    <Link to="/registration">S'enregistrer</Link>
+                </div>
+                <hr></hr>
+                <div className="login__main__signIn">
+                    <p>Se connecter</p>
+                    <div>
+                        <p>Identifiant :</p>
+                        <label>
+                            <input
+                                type="text"
+                                value={identifierValue}
+                                placeholder="Veuillez renseigner votre identifiant"
+                                onChange={(event) =>
+                                    setIdentifierValue(event.target.value)
+                                }
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <p>Mot de passe :</p>
+                        <label>
+                            <input
+                                type="text"
+                                value={passwordValue}
+                                placeholder="Veuillez renseigner votre identifiant"
+                                onChange={(event) =>
+                                    setPasswordValue(event.target.value)
+                                }
+                            />
+                        </label>
+                    </div>
+                    <button onClick={() => setActionLogin(true)}>
+                        Valider
+                    </button>
+                </div>
+            </div>
         </div>
-        <hr></hr>
-        <div className='login__main__signIn'>
-          <p>Se connecter</p>
-          <div>
-            <p>Identifiant :</p>
-            <label>
-              <input
-                type="text"
-                value={identifierValue}
-                placeholder="Veuillez renseigner votre identifiant"
-                onChange={(event) => setIdentifierValue(event.target.value)}
-              />
-            </label>
-          </div>
-          <div>
-            <p>Mot de passe :</p>
-            <label>
-              <input
-                type="text"
-                value={passwordValue}
-                placeholder="Veuillez renseigner votre identifiant"
-                onChange={(event) => setPasswordValue(event.target.value)}
-              />
-            </label>
-          </div>
-          <button onClick={() => setActionLogin(true)}>Valider</button>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
