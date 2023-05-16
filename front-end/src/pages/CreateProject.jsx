ca948130aa9cfcb75ca24bdb7efad18030e9a13a
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-05-16 19:19:08 +0200

    Update CreateProject.jsx
    Code format improved

--- front-end/src/pages/CreateProject.jsx
+++ front-end/src/pages/CreateProject.jsx
@@ -3,72 +3,76 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function CreateProject() {
+    const navigate = useNavigate()
+    const [nameOfProject, setNameOfProject] = useState('')
+    const [deadLineDate, setDeadLineDate] = useState('')
+    const [descriptionOfProject, setDescriptionOfProject] = useState('')
+    const [actionCreateProject, setActionCreateProject] = useState(false)
+    const token = localStorage.getItem('TOKEN')
+    let source = axios.CancelToken.source()
 
-  const navigate = useNavigate()
-  const [nameOfProject, setNameOfProject] = useState('')
-  const [deadLineDate, setDeadLineDate] = useState('')
-  const [descriptionOfProject, setDescriptionOfProject] = useState('')
-  const [actionCreateProject, setActionCreateProject] = useState(false)
-  const token = localStorage.getItem("TOKEN")
-  let source = axios.CancelToken.source()
+    const API_URL_Plano = 'http://localhost:3000/plano/projects'
 
-  const API_URL_Plano = 'http://localhost:3000/plano/projects'
+    useEffect(() => {
+        if (actionCreateProject) {
+            axios.post(API_URL_Plano, {
+                cancelToken: source.token,
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                name: nameOfProject,
+                dateDeadLine: deadLineDate,
+                description: descriptionOfProject,
+            })
+            setActionCreateProject(false)
+            navigate('/')
+            window.location.reload()
+        }
+    }, [actionCreateProject])
 
-  useEffect(() => {
-    if (actionCreateProject) {
-      axios
-        .post(API_URL_Plano, {
-          cancelToken: source.token,
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          name: nameOfProject,
-          dateDeadLine: deadLineDate,
-          description: descriptionOfProject
-        })
-      setActionCreateProject(false)
-      navigate("/")
-      window.location.reload()
-    }
-  }, [actionCreateProject])
-
-  return (
-    <div className="createProject">
-      <h2>Créer un projet</h2>
-      <div>
-        <p>Nom du project :</p>
-        <label>
-          <input
-            type="text"
-            value={nameOfProject}
-            placeholder="Le nom de votre projet"
-            onChange={(event) => setNameOfProject(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <p>Date du rendu :</p>
-        <label>
-          <input
-            type="text"
-            value={deadLineDate}
-            placeholder="La date de rendu de votre projet"
-            onChange={(event) => setDeadLineDate(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <p>Description :</p>
-        <label>
-          <input
-            type="text"
-            value={descriptionOfProject}
-            placeholder="la description de votre projet"
-            onChange={(event) => setDescriptionOfProject(event.target.value)}
-          />
-        </label>
-      </div>
-      <button onClick={() => setActionCreateProject(true)}>Créer</button>
-    </div>
-  )
+    return (
+        <div className="createProject">
+            <h2>Créer un projet</h2>
+            <div>
+                <p>Nom du project :</p>
+                <label>
+                    <input
+                        type="text"
+                        value={nameOfProject}
+                        placeholder="Le nom de votre projet"
+                        onChange={(event) =>
+                            setNameOfProject(event.target.value)
+                        }
+                    />
+                </label>
+            </div>
+            <div>
+                <p>Date du rendu :</p>
+                <label>
+                    <input
+                        type="text"
+                        value={deadLineDate}
+                        placeholder="La date de rendu de votre projet"
+                        onChange={(event) =>
+                            setDeadLineDate(event.target.value)
+                        }
+                    />
+                </label>
+            </div>
+            <div>
+                <p>Description :</p>
+                <label>
+                    <input
+                        type="text"
+                        value={descriptionOfProject}
+                        placeholder="la description de votre projet"
+                        onChange={(event) =>
+                            setDescriptionOfProject(event.target.value)
+                        }
+                    />
+                </label>
+            </div>
+            <button onClick={() => setActionCreateProject(true)}>Créer</button>
+        </div>
+    )
 }
