ea2db6f60b7375c474992a110748089759ada2f8
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-05-17 19:34:16 +0200

    Update Title.jsx
    Code format improved

--- front-end/src/components/Title.jsx
+++ front-end/src/components/Title.jsx
@@ -1,10 +1,5 @@
-import React from "react";
+import React from 'react'
 
 export default function Title() {
-  return (
-    <h1
-      className="title-app">
-      P|_A|\|O
-    </h1>
-  )
+    return <h1 className="title-app">P|_A|\|O</h1>
 }
