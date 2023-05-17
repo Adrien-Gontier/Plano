e17dfe6a919606d014ab75803991afe52b5e5952
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-05-17 19:34:19 +0200

    Update MiniTask.jsx
    Code format improved

--- front-end/src/components/MiniTask.jsx
+++ front-end/src/components/MiniTask.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 export default function MiniTask({ props }) {
     return (
         <div>
-            <p>
-                {props.name}
-            </p>
+            <p>{props.name}</p>
         </div>
     )
 }
