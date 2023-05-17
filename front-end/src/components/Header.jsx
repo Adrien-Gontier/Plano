01c98263e49b2b780ea149e1345c252425fda5f3
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-05-17 19:34:10 +0200

    Update Header.jsx
    Code format improved

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -3,14 +3,11 @@ import DeconnectBtn from './DeconnectBtn'
 import Title from './Title'
 
 export default function Header(props) {
-  const { isLogged, setIsLogged } = props
-  return (
-    <div className="header">
-      <Title />
-      <DeconnectBtn
-        isLogged={isLogged}
-        setIsLogged={setIsLogged}
-      />
-    </div>
-  )
+    const { isLogged, setIsLogged } = props
+    return (
+        <div className="header">
+            <Title />
+            <DeconnectBtn isLogged={isLogged} setIsLogged={setIsLogged} />
+        </div>
+    )
 }
